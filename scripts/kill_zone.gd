9e219ad8f30f816861730106888c344b3f1ce7e9
MustafaTahaCetin <=>
2025-09-27 12:05:56 +0300

    news added

--- scripts/kill_zone.gd
+++ scripts/kill_zone.gd
@@ -2,8 +2,14 @@ extends Area2D
 
 
 
-
+@onready var timer = $Timer
 
 
 func _on_body_entered(body: CharacterBody2D) -> void:
 	print("you died")
+	timer.start()
+	
+	
+
+func _on_timer_timeout() -> void:
+	get_tree().reload_current_scene()
