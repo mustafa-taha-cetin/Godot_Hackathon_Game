4eafc1b5ae262761086c2d5dc32aeec76279eade
MustafaTahaCetin <=>
2025-09-27 14:13:05 +0300

    V2.2

--- scripts/kill_zone.gd
+++ scripts/kill_zone.gd
@@ -7,9 +7,11 @@ extends Area2D
 
 func _on_body_entered(body: CharacterBody2D) -> void:
 	print("you died")
+	Engine.time_scale = 0.4
 	timer.start()
 	
 	
 
 func _on_timer_timeout() -> void:
+	Engine.time_scale = 1
 	get_tree().reload_current_scene()
--- scripts/slime.gd
+++ scripts/slime.gd
@@ -8,15 +8,17 @@ var direction = RIGHT
 
 @onready var ray_cast_right = $RayCastRight 
 @onready var ray_cast_left = $RayCastLeft
-
+@onready var animated_sprite = $AnimatedSprite2D
 
 
 func _process(delta):
 	
 	if ray_cast_right.is_colliding():
 		direction = LEFT
+		animated_sprite.flip_h = true
 		
 	if ray_cast_left.is_colliding():
 		direction = RIGHT
+		animated_sprite.flip_h = false
 	
 	position.x += direction * SPEED * delta
